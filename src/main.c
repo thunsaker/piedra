a47af1950f08c3e9a0f58ad42cfaf85091253abe
Thomas Hunsaker <thunsaker@users.noreply.github.com>
2013-11-06 15:14:05 -0800

    Updated sample file to SDK 2.

--- src/main.c
+++ src/main.c
@@ -1,29 +1,44 @@
-#include "pebble_os.h"
-#include "pebble_app.h"
-#include "pebble_fonts.h"
+#include "pebble.h"
 
+static Window *window;
 
-#define MY_UUID { 0x01, 0xEA, 0x0B, 0x3C, 0x20, 0x53, 0x4C, 0xCB, 0x95, 0xBC, 0x5F, 0x23, 0x05, 0x56, 0x04, 0xDB }
-PBL_APP_INFO(MY_UUID,
-             "Template App", "Your Company",
-             1, 0, /* App version */
-             DEFAULT_MENU_ICON,
-             APP_INFO_STANDARD_APP);
+static TextLayer *text_layer;
 
-Window window;
-
-
-void handle_init(AppContextRef ctx) {
-  (void)ctx;
-
-  window_init(&window, "Window Name");
-  window_stack_push(&window, true /* Animated */);
+void up_single_click_handler(ClickRecognizerRef recognizer, void *context) {
+  vibes_short_pulse();
 }
 
+void down_single_click_handler(ClickRecognizerRef recognizer, void *context) {
+  vibes_long_pulse();
+}
 
-void pbl_main(void *params) {
-  PebbleAppHandlers handlers = {
-    .init_handler = &handle_init
-  };
-  app_event_loop(params, &handlers);
+void select_single_click_handler(ClickRecognizerRef recognizer, void *context) {
+  vibes_double_pulse();
+}
+
+void click_config_provider(void *context) {
+  const uint16_t repeat_interval_ms = 1000;
+  window_single_repeating_click_subscribe(BUTTON_ID_SELECT, repeat_interval_ms, select_single_click_handler);
+  window_single_repeating_click_subscribe(BUTTON_ID_UP, repeat_interval_ms, up_single_click_handler);
+  window_single_repeating_click_subscribe(BUTTON_ID_DOWN, repeat_interval_ms, down_single_click_handler);
+}
+
+int main(void) {
+  window = window_create();
+  window_stack_push(window, true /* Animated */ );
+
+  window_set_click_config_provider(window, click_config_provider);
+
+  Layer *window_layer = window_get_root_layer(window);
+  GRect bounds = layer_get_frame(window_layer);
+  text_layer = text_layer_create(bounds);
+  text_layer_set_text(text_layer, "Press buttons for different vibes. Hold for repeating.");
+  text_layer_set_font(text_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD));
+  text_layer_set_text_alignment(text_layer, GTextAlignmentCenter);
+  layer_add_child(window_layer, text_layer_get_layer(text_layer));
+
+  app_event_loop();
+
+  text_layer_destroy(text_layer);
+  window_destroy(window);
 }
